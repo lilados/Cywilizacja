c6023ea79dbed11842ff5595738367bc8f03ee4b
Fabian Bacher <75790801+Harickss@users.noreply.github.com>
2024-06-02 18:09:39 +0200

    latest baza danych ok
    dsds

--- cywilizacja.sql
+++ cywilizacja.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Maj 31, 2024 at 08:53 PM
+-- Host: localhost
+-- Generation Time: Cze 02, 2024 at 06:08 PM
 -- Wersja serwera: 10.4.32-MariaDB
 -- Wersja PHP: 8.2.12
 
@@ -94,7 +94,10 @@ INSERT INTO `handel` (`id`, `towar`, `ilosc`, `cena`, `partner_handlowy`, `panst
 (13, 'drewno', 3000, 500.00, 'Wronis', 1),
 (14, 'drewno', 500, 300.00, 'Wronis', 1),
 (15, 'drewno', 500, 300.00, 'Wronis', 1),
-(16, 'drewno', 500, 300.00, 'Wronis', 1);
+(16, 'drewno', 500, 300.00, 'Wronis', 1),
+(17, 'kamien', 400, 200.00, 'Masuria', 1),
+(18, 'zywnosc', 200, 600.00, 'Wronis', 1),
+(19, 'kamien', 20, 50.00, 'Wronis', 1);
 
 -- --------------------------------------------------------
 
@@ -115,25 +118,14 @@ CREATE TABLE `jednostki_wojskowe` (
 --
 
 INSERT INTO `jednostki_wojskowe` (`id`, `rodzaj`, `liczebnosc`, `stan_gotowosci`, `panstwo_id`) VALUES
-(2, 'Kawalerzysta', 233, 'defend', 1),
-(3, 'Łucznik', 10, 'defend', 1),
-(4, 'Rekrut', 5, 'ready', 1),
-(5, 'Rekrut', 5, 'ready', 1),
-(6, 'Rekrut', 5, 'ready', 1),
-(7, 'Rekrut', 5, 'ready', 1),
-(8, 'Rekrut', 2, 'ready', 1),
-(9, 'Rekrut', 3, 'ready', 1),
-(10, 'Rekrut', 3, 'ready', 1),
-(11, 'Rekrut', 2, 'ready', 1),
-(12, 'Rekrut', 3, 'ready', 1),
-(13, 'Rekrut', 4, 'ready', 1),
-(14, 'Rekrut', 3, 'ready', 1),
-(15, 'Łucznik', 20, 'ready', 1),
-(16, 'Rekrut', 15, 'ready', 1),
-(17, 'Rekrut', 10, 'ready', 1),
-(18, 'Rekrut', 30, 'ready', 1),
-(19, 'Rekrut', 20, 'ready', 3),
-(20, 'Rekrut', 3, 'ready', 1);
+(24, 'Rekrut', 50, 'Gotowy na front', 1),
+(25, 'Rekrut', 50, 'Gotowy na front', 1),
+(26, 'Kawalerzysta', 200, 'Gotowy na front', 1),
+(27, 'Rekrut', 20, 'Gotowy na front', 2),
+(28, 'Rekrut', 10, 'Gotowy na front', 3),
+(29, 'Rekrut', 20, 'Obrona państwa', 1),
+(30, 'Łucznik', 10, 'Gotowy na front', 4),
+(31, 'Strażnik', 10, 'Obrona państwa', 1);
 
 -- --------------------------------------------------------
 
@@ -197,7 +189,8 @@ CREATE TABLE `polityki` (
 --
 
 INSERT INTO `polityki` (`id`, `nazwa`, `typ`, `opis`, `panstwo_id`) VALUES
-(1, 'Uczelnictwo nr 3', 'edukacyjna', 'Zniesienie potrzeby uczenia się?', 1);
+(1, 'Uczelnictwo nr 3', 'edukacyjna', 'Zniesienie potrzeby uczenia się?', 1),
+(2, 'ds', 'ekonomiczna', 'dsds', 1);
 
 -- --------------------------------------------------------
 
@@ -285,7 +278,40 @@ INSERT INTO `projekty_infrastrukturalne` (`id`, `nazwa`, `ilosc`, `typ`, `panstw
 (2, 'asdwdad', 300, 'dom', 1),
 (3, 'domy', 20, 'dom', 1),
 (4, 'Drogi!', 34, 'droga', 1),
-(5, 'Domy', 32, 'droga', 1);
+(5, 'Domy', 32, 'droga', 1),
+(6, 'DOm', 2, 'dom', 3),
+(7, 'Drogi', 1, 'droga', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `relations`
+--
+
+CREATE TABLE `relations` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `panstwo1` varchar(50) DEFAULT NULL,
+  `panstwo2` varchar(50) DEFAULT NULL,
+  `relation` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `relations`
+--
+
+INSERT INTO `relations` (`id`, `panstwo1`, `panstwo2`, `relation`) VALUES
+(1, 'Masuria', 'Janczland', 'Sojusznicy'),
+(2, 'Masuria', 'Underwoodland', 'Wrogowie'),
+(3, 'Masuria', 'Wronis', 'Neutralni'),
+(4, 'Janczland', 'Masuria', 'Sojusznicy'),
+(5, 'Janczland', 'Underwoodland', 'Sojusznicy'),
+(6, 'Janczland', 'Wronis', 'sojusznicy'),
+(7, 'Underwoodland', 'Masuria', 'Wrogowie'),
+(8, 'Underwoodland', 'Janczland', 'Sojusznicy'),
+(9, 'Underwoodland', 'Wronis', 'Neutralni'),
+(10, 'Wronis', 'Masuria', 'Wrogowie'),
+(11, 'Wronis', 'Janczland', 'Neutralni'),
+(12, 'Wronis', 'Underwoodland', 'Sojusznicy');
 
 -- --------------------------------------------------------
 
@@ -368,71 +394,55 @@ CREATE TABLE `zasoby` (
 --
 
 INSERT INTO `zasoby` (`id`, `typ`, `ilosc`, `panstwo_id`) VALUES
-(58, 'zywnosc', 0, 1),
-(59, 'kamien', 0, 1),
-(60, 'drewno', 0, 1),
-(61, 'wegiel', 0, 1),
-(62, 'zloto', 0, 1),
-(63, 'kasa', 0, 1),
-(64, 'zelazo', 0, 1),
-(65, 'zywnosc', 0, 2),
-(66, 'kamien', 0, 2),
-(67, 'drewno', 0, 2),
-(68, 'wegiel', 0, 2),
-(69, 'zloto', 0, 2),
-(70, 'kasa', 0, 2),
-(71, 'zelazo', 0, 2),
-(72, 'zywnosc', 0, 3),
-(73, 'kamien', 0, 3),
-(74, 'drewno', 0, 3),
-(75, 'wegiel', 0, 3),
-(76, 'zloto', 0, 3),
-(77, 'kasa', 0, 3),
-(78, 'zelazo', 0, 3),
-(79, 'zywnosc', 0, 4),
-(80, 'kamien', 0, 4),
-(81, 'drewno', 0, 4),
-(82, 'wegiel', 0, 4),
-(83, 'zloto', 0, 4),
-(84, 'kasa', 0, 4),
-(85, 'zelazo', 0, 4),
-(86, 'zywnosc', 1000, 1),
-(87, 'zywnosc', -300, 1),
-(88, 'zywnosc', 300, 3),
-(89, 'zywnosc', -200, 3),
-(90, 'zywnosc', -25, 1),
-(91, 'zywnosc', 25, 2),
-(92, 'kamien', -20, 1),
-(93, 'kamien', 20, 2),
-(94, 'zywnosc', 20, 1),
-(95, 'zywnosc', -20, 2),
-(96, 'kamien', 30, 1),
-(97, 'kamien', -30, 2),
-(98, 'kamien', -10, 1),
-(99, 'kamien', 10, 2),
-(100, 'drewno', 300, 1),
-(101, 'drewno', -20, 1),
-(102, 'drewno', 20, 2),
-(103, 'zywnosc', -600, 1),
-(104, 'zywnosc', -30, 1),
-(105, 'drewno', -200, 1),
-(106, 'drewno', 200, 2),
-(107, 'drewno', 4000, 1),
-(108, 'drewno', -640, 1),
-(109, 'kasa', 3000, 2),
-(110, 'drewno', -300, 1),
-(111, 'drewno', 300, 2),
-(112, 'kasa', -500, 2),
-(113, 'drewno', 3000, 2),
-(114, 'kasa', -300, 2),
-(115, 'drewno', 500, 2),
-(116, 'drewno', -500, 1),
-(117, 'kasa', -300, 2),
-(118, 'drewno', 500, 2),
-(119, 'drewno', -500, 1),
-(120, 'kasa', -300, 2),
-(121, 'kasa', 300, 1),
-(122, 'drewno', 500, 2);
+(140, 'zywnosc', 200, 1),
+(141, 'kamien', 450, 1),
+(142, 'drewno', 2000, 1),
+(143, 'wegiel', 100, 1),
+(144, 'zloto', 200, 1),
+(145, 'kasa', 6000, 1),
+(146, 'zelazo', 200, 1),
+(147, 'zywnosc', 50, 2),
+(148, 'kamien', 5000, 2),
+(149, 'drewno', 20, 2),
+(150, 'wegiel', 1000, 2),
+(151, 'zloto', 2000, 2),
+(152, 'kasa', 10000, 2),
+(153, 'zelazo', 5000, 2),
+(154, 'zywnosc', 500, 3),
+(155, 'kamien', 600, 3),
+(156, 'drewno', 400, 3),
+(157, 'wegiel', 500, 3),
+(158, 'zloto', 600, 3),
+(159, 'kasa', 8000, 3),
+(160, 'zelazo', 200, 3),
+(161, 'zywnosc', 400, 4),
+(162, 'kamien', 500, 4),
+(163, 'drewno', 500, 4),
+(164, 'wegiel', 200, 4),
+(165, 'zloto', 500, 4),
+(166, 'kasa', 400, 4),
+(167, 'zelazo', 600, 4),
+(168, 'zelazo', 500, 1),
+(169, 'kamien', -400, 1),
+(170, 'kasa', -200, 3),
+(171, 'kasa', 200, 1),
+(172, 'kamien', 400, 3),
+(173, 'zywnosc', -100, 4),
+(174, 'zywnosc', 10, 1),
+(175, 'zywnosc', 20, 1),
+(176, 'zywnosc', 200, 1),
+(177, 'zywnosc', -200, 1),
+(178, 'kasa', -600, 2),
+(179, 'kasa', 600, 1),
+(180, 'zywnosc', 200, 2),
+(181, 'zywnosc', 600, 2),
+(182, 'drewno', -40, 3),
+(183, 'zywnosc', -100, 1),
+(184, 'kamien', -20, 1),
+(185, 'kasa', -50, 2),
+(186, 'kasa', 50, 1),
+(187, 'kamien', 20, 2),
+(188, 'drewno', -20, 3);
 
 -- --------------------------------------------------------
 
@@ -536,6 +546,12 @@ ALTER TABLE `projekty_infrastrukturalne`
   ADD KEY `panstwo_id` (`panstwo_id`);
 
 --
+-- Indeksy dla tabeli `relations`
+--
+ALTER TABLE `relations`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indeksy dla tabeli `wskazniki`
 --
 ALTER TABLE `wskazniki`
@@ -590,13 +606,13 @@ ALTER TABLE `bilans`
 -- AUTO_INCREMENT for table `handel`
 --
 ALTER TABLE `handel`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `jednostki_wojskowe`
 --
 ALTER TABLE `jednostki_wojskowe`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `operacje_wojskowe`
@@ -614,7 +630,7 @@ ALTER TABLE `panstwo`
 -- AUTO_INCREMENT for table `polityki`
 --
 ALTER TABLE `polityki`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `postepy_badawcze`
@@ -638,7 +654,13 @@ ALTER TABLE `projekty_badawcze`
 -- AUTO_INCREMENT for table `projekty_infrastrukturalne`
 --
 ALTER TABLE `projekty_infrastrukturalne`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `relations`
+--
+ALTER TABLE `relations`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `wskazniki`
@@ -662,7 +684,7 @@ ALTER TABLE `wyniki_badawcze`
 -- AUTO_INCREMENT for table `zasoby`
 --
 ALTER TABLE `zasoby`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=123;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=189;
 
 --
 -- AUTO_INCREMENT for table `zasoby_infrastrukturalne`
